9fcf39e93082888032c921b4fc5f593e8ce8243d
Chloé Grimaldi <chloegrimaldi@hotmail.fr>
2024-11-27 13:14:21 +0000

    reste

--- models/intermediate/colonne_velos_casses.sql
+++ models/intermediate/colonne_velos_casses.sql
@@ -1,9 +1,14 @@
-SELECT*,
+with s as (SELECT*,
 CASE 
-    WHEN (duration_minute < 2 AND departure_latitude=arrival_latitude)
+    WHEN (formatted_duration<"00:01:16")
     THEN 1
     
 ELSE 0 
 END AS trajet_defectueux
 
 FROM {{ ref('stg_donnees_juin__donnes_velib_juin') }}
+) 
+SELECT *
+FROM s 
+ORDER BY trajet_defectueux desc, formatted_duration desc
+--formatted_duration < "00:01:15" AND arrival_latitude = departure_latitude
