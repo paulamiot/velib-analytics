41653f59417c1ad3f63075ca526e590cf9517c7d
Corentin Fau <faucorentin.bidon@gmail.com>
2024-11-28 11:53:12 +0000

    creation table ratio

---
+++ models/intermediate/ratio_tension_station_heure.sql
@@ -0,0 +1,37 @@
+WITH uid_etats AS (
+  SELECT 
+    station_id,
+    heure,
+    station_id,
+    station_nom,
+    station_statut,
+    station_capacite,
+    nb_velo_elec,
+    nb_velo_meca,
+    CONCAT(station_id,heure) AS unique_id_heure
+  FROM {{ ref('stg_donnees_juin__etats_stations_par_heure') }}
+  ),
+uid_depart AS (
+  SELECT
+    station_id,
+    hour,
+    CONCAT(station_id,hour) AS unique_id_heure,
+    nb_depart_cumule,
+    nb_depart_moyen
+  FROM {{ ref('depart_station_heure_arrondissement') }}
+)
+SELECT 
+    unique_id_heure AS cle_unique,
+    uid_depart.station_id,
+    heure,
+    station_nom,
+    station_statut,
+    nb_velo_elec,
+    nb_velo_meca,
+    station_capacite,
+    (nb_velo_elec + nb_velo_meca) AS nb_velo_dispo,
+    nb_depart_moyen,
+    ROUND ((1 - (SAFE_DIVIDE(nb_depart_moyen, (nb_velo_elec + nb_velo_meca)))),2)AS ratio_tension
+FROM uid_etats 
+LEFT JOIN uid_depart
+  USING (unique_id_heure)
