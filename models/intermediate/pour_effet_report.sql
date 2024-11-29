b1a652aa7fd9773cb8c3b89436d0448a90612ca5
Paul Amiot <paul.velibanalytics@gmail.com>
2024-11-29 13:40:13 +0000

    machin

--- models/intermediate/pour_effet_report.sql
+++ models/intermediate/pour_effet_report.sql
@@ -1,12 +1,20 @@
-select
-    b.*,
+WITH sub1 AS (
+
+    SELECT
+        a.*,
+        CONCAT(station_id, heure) AS cle_unique, 
+    FROM {{ ref("stg_donnees_juin__etats_stations_par_heure_v2") }} as a
+)
+
+SELECT 
+    sub1.cle_unique, 
+    sub1.* EXCEPT (cle_unique),
     nb_depart_moyen,
     ratio_tension,
     nb_stations_arr,
     nbre_bornettes_arr,
     commune_arrondissement,
     ratio_category
-from {{ ref("station_ratio_horaire_arrondissement") }} as a
-left join
-    {{ ref("stg_donnees_juin__etats_stations_par_heure_v2") }} as b
-    on a.station_id = b.station_id
+FROM sub1
+LEFT JOIN {{ ref("station_ratio_horaire_arrondissement") }} as b
+    USING (cle_unique)
