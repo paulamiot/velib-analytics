6bf6080ed48eaf5f595a1a9fd16226f779cbee19
Paul Amiot <paul.velibanalytics@gmail.com>
2024-11-29 13:27:05 +0000

    join pour effet report

--- models/intermediate/pour_effet_report.sql
+++ models/intermediate/pour_effet_report.sql
@@ -1,2 +1,12 @@
-SELECT *
-FROM {{ ref('stg_donnees_juin__info_arrondissement_2') }}
+select
+    b.*,
+    nb_depart_moyen,
+    ratio_tension,
+    nb_stations_arr,
+    nbre_bornettes_arr,
+    commune_arrondissement,
+    ratio_category
+from {{ ref("station_ratio_horaire_arrondissement") }} as a
+left join
+    {{ ref("stg_donnees_juin__etats_stations_par_heure_v2") }} as b
+    on a.station_id = b.station_id
